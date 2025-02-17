2da62361596affaafaf6d20fc0d287f38a183030
fukusuke-navagis <kosuke.fukuda@navagis.com>
2025-02-18 01:05:34 +0900

    feat: add keymap to switch buffers
    <C-n> -> :bnext, <C-b> :bprev

--- init.lua
+++ init.lua
@@ -189,6 +189,10 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- My custom keymap to swithch the buffers
+vim.keymap.set('n', '<C-n>', ':bnext<CR>')
+vim.keymap.set('n', '<C-b>', ':bprev<CR>')
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
