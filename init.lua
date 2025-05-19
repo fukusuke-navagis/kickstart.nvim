ea0e0f071099a8edea9df39d8187fd492cae4cb2
Kosuke Fukuda <jugglestar@Kosukes-MacBook-Air.local>
2025-05-19 11:18:27 +0900

    Change: Enable Nerd Font

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
@@ -289,7 +289,7 @@ require('lazy').setup({
   },
 
   { -- Adds Neo Scroll: a smooth scroll plug-in
-    'karb94/neoscroll.nvim', 
+    'karb94/neoscroll.nvim',
     opts = {},
   },
 
